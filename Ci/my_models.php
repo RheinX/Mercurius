a5f79966bd116b8afa444b886339e3f4e77bace5
RheinX <747972298@qq.com>
2017-05-11 22:13:11 +0800

    5.11

---
+++ Ci/my_models.php
@@ -0,0 +1,32 @@
+<?php die();
+
+/**
+ * Add you custom models here that you are loading in your controllers
+ *
+ * <code>
+ * $this->site_model->get_records()
+ * </code>
+ * Where site_model is the model Class
+ *
+ * ---------------------- Models to Load ----------------------
+ * <examples>
+ *
+ * @property membership_model $membership_model
+ * @property site_model       $site_model
+ * @property data_model       $data_model
+ * @property products_model   $products_model
+ * @property film_model       $film_model
+ * @property film_model1      $film_model1
+ *
+ *@property User_model      $User_model
+ * @property Label_model  $Label_model
+ * @property Announcement_model $Announcement_model
+ * @property Author_model  $Author_model
+ * @property Book_model $Book_model
+ *
+ */
+class my_models
+{
+}
+
+// End my_models.php
